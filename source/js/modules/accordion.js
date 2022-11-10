5128fcb7d0a90efe87310e4569cc9555a8639ff1
Kate Kulinich <kulinich.ekaterina.andreevna@gmail.com>
2022-11-10 04:08:26 +0100

    corrceting accordion

--- source/js/modules/accordion.js
+++ source/js/modules/accordion.js
@@ -1,18 +1,6 @@
 const body = document.querySelector('body');
 const accordionTitles = document.querySelectorAll('[data-footer="accordion"]');
 
-// const accordionHandler = (item) => {
-//   const parent = item.parentNode;
-//   if (parent.classList.contains('is-open')) {
-//     parent.classList.remove('is-open');
-//   } else {
-//     document.querySelectorAll('.is-open').forEach((child) => {
-//       child.classList.remove('is-open');
-//     });
-//     parent.classList.add('is-open');
-//   }
-// };
-
 const accordionHandler = (item) => {
   const parent = item.parentNode;
   if (body.clientWidth >= 375) {
